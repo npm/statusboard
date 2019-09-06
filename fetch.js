ed83bbe889236c5bd51a39beea29d43e4197b2a8
Darcy Clarke <darcy@darcyclarke.me>
2019-09-06 00:48:00 -0400

    Remove archived repos

--- fetch.js
+++ fetch.js
@@ -3,6 +3,6 @@
   const fs = require('fs')
   const query = fs.readFileSync('./query.gql', 'utf8')
   const { graphql } = require('@octokit/graphql')
-  const { organization } = await graphql(query, { headers: { authorization: `token ${process.env.AUTH_TOKEN}` }})
-  fs.writeFileSync('./dump.json', JSON.stringify(organization), 'utf8')
+  const response = await graphql(query, { headers: { authorization: `token ${process.env.AUTH_TOKEN}` }})
+  fs.writeFileSync('./dump.json', JSON.stringify(response), 'utf8')
 })()
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
-const repos = require('./repositories')
-const { repositories } = require('./dump')
+const { organization } = require('./dump')
 const copyfiles = require('copyfiles')
 const Handlebars = require('handlebars')
 const source = fs.readFileSync('./template.hbs', 'utf8')
@@ -12,7 +11,8 @@ copyfiles(['./src/*.js', './src/*.css', './dist/'], (err) => {
   console.log('files copied!')
 })
 
-fs.writeFile(file, template({ repos: repositories.nodes }), 'utf8', (err) => {
+const repos = organization.repositories.nodes.filter(r => !r.isArchived)
+fs.writeFile(file, template({ repos }), 'utf8', (err) => {
   if (err) throw err
   console.log(`${file} was created!`)
 })
