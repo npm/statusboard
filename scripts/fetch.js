805ff2945aaf734447ad3cd97b318080e0af6759
Darcy Clarke <darcy@darcyclarke.me>
2019-11-05 14:08:58 -0500

    chore: remove console.log

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -49,7 +49,6 @@
               let response = await fetch(`https://raw.githack.com/npm/${r.name}/master/package.json`)
               let pkg = await response.json()
               r.prs_count = prs.data.length
-              console.log('>>>>', prs.data.length, issues.data.length)
               r.issues_count = issues.data.length
               r.build_status = status.data[0].state
               r.node = pkg && pkg.engines && pkg.engines.node ? pkg.engines.node : null
