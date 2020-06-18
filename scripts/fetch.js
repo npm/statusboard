15182919319c2b98dc7480fb7ff7a0cd4f8185de
Darcy Clarke <darcy@darcyclarke.me>
2020-06-18 02:33:26 -0400

    chore: fix up fetching & error handling

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -5,6 +5,7 @@
   const { sleep } = require('sleepover')
   const fetch = require('node-fetch')
   const mkdirp = require('mkdirp')
+  const moment = require('moment')
   const { Octokit } = require('@octokit/rest')
   const octokit = new Octokit({ auth: process.env.AUTH_TOKEN })
   const now = new Date()
@@ -33,42 +34,69 @@
           repositories = repositories.filter(r => !r.archived)
           Promise.all(repositories.map(async r => {
             try {
-              let status = await octokit.repos.listStatusesForRef({
-                owner: 'npm',
-                repo: r.name,
-                ref: r.default_branch
-              })
+
+              let response
+              let pkg
+              let stats
+              let data
+
               let prs = await octokit.pulls.list({
                 owner: 'npm',
                 repo: r.name,
                 per_page: 100
               })
+              r.prs_count = prs.data.length || 0
+
               let issues = await octokit.issues.listForRepo({
                 owner: 'npm',
                 repo: r.name,
                 per_page: 100
               })
-              let response = await fetch(`https://coveralls.io/github/npm/${r.name}.json`)
-              let stats = await response.json()
-              r.coverage = Math.round(stats.covered_percent) || ''
-              r.coverageLevel = r.coverage ? (r.coverage === 100) ? 'high' : (r.coverage > 80) ? 'medium' : 'low' : ''
-              response = await fetch(`https://api.npmjs.org/downloads/point/last-month/${r.name}`)
-              let data = await response.json()
-              r.downloads = data.downloads
-              response = await fetch(`https://raw.githack.com/npm/${r.name}/master/package.json`)
-              let pkg = await response.json()
-              r.prs_count = prs.data.length || 0
               r.issues_count = (issues.data.length || 0) - r.prs_count
-              r.build_status = status.data[0].state
+
+              r.pushed_at_diff = moment(r.pushed_at).fromNow()
+              console.log(r.pushed_at_diff)
+
+              let status = await octokit.repos.listStatusesForRef({
+                owner: 'npm',
+                repo: r.name,
+                ref: r.default_branch
+              })
+              r.build_status = status && status.data && status.data[0] ? status.data[0].state : ''
+
+              try {
+                response = await fetch(`https://coveralls.io/github/npm/${r.name}.json`)
+                stats = await response.json()
+              } catch (e) {
+                console.error(e)
+              }
+              r.coverage = stats ? Math.round(stats.covered_percent) : ''
+              r.coverageLevel = r.coverage ? (r.coverage === 100) ? 'high' : (r.coverage > 80) ? 'medium' : 'low' : ''
+
+              try {
+                response = await fetch(`https://raw.githack.com/npm/${r.name}/${r.default_branch}/package.json`)
+                pkg = await response.json()
+                if (pkg && pkg.name) {
+                  response = await fetch(`https://api.npmjs.org/downloads/point/last-month/${pkg.name}`)
+                  data = await response.json()
+                }
+              } catch (e) {
+                console.error(e)
+              }
               r.node = pkg && pkg.engines && pkg.engines.node ? pkg.engines.node : null
-              r.license.key = pkg.license || r.license.key
-              r.version = pkg.version
+              r.license.key = pkg ? pkg.license || r.license.key : null
+              r.version = pkg ? pkg.version : null
+              r.downloads = data && data.downloads ? data.downloads : 0
+
               return r
+
             } catch (e) {
-              //console.log(e)
+              console.error(e)
             }
+
             return r
-          })).then(cb)
+
+          })).then(cb).catch(e => console.error(e))
         } else {
           page++
           repositories = repositories.concat(data)
