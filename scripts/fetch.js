f627761e11707df8cc94accfc1cc9326e70b14f6
Darcy Clarke <darcy@darcyclarke.me>
2020-07-17 00:15:06 -0400

    fix: fetch issues

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -6,6 +6,7 @@
   const fetch = require('node-fetch')
   const mkdirp = require('mkdirp')
   const moment = require('moment')
+  const repositories = require('../data/maintained.json')
   const { Octokit } = require('@octokit/rest')
   const octokit = new Octokit({ auth: process.env.AUTH_TOKEN })
   const now = new Date()
@@ -13,97 +14,67 @@
   const dest = `../data/${now.getUTCFullYear()}/${month}/${now.getUTCDate()}.json`
   const latest = `../data/latest.json`
   const dir = dest.split('/').slice(0, -1).join('/')
-  let repositories = []
   function writeFile (data) {
-    mkdirp(path.resolve(__dirname, dir), function (err) {
-      if (err) console.error(err)
+    console.log(data)
+    mkdirp(path.resolve(__dirname, dir)).then(() => {
       fs.writeFileSync(path.resolve(__dirname, dest), JSON.stringify(data), 'utf8')
       fs.writeFileSync(path.resolve(__dirname, latest), JSON.stringify(data), 'utf8')
-    })
+    }).catch(err => console.error(err))
   }
-  function run (page, cb) {
-    page = page || 1
-    octokit.repos
-      .listForOrg({
-        org: 'npm',
-        type: 'public',
-        per_page: 100,
-        page: page
-      }).then(({ data }) => {
-        if (!data || !data.length) {
-          repositories = repositories.filter(r => !r.archived)
-          Promise.all(repositories.map(async r => {
-            try {
-
-              let response
-              let pkg
-              let stats
-              let data
-
-              let prs = await octokit.pulls.list({
-                owner: 'npm',
-                repo: r.name,
-                per_page: 100
-              })
-              r.prs_count = prs.data.length || 0
-
-              let issues = await octokit.issues.listForRepo({
-                owner: 'npm',
-                repo: r.name,
-                per_page: 100
-              })
-              r.issues_count = (issues.data.length || 0) - r.prs_count
-
-              r.pushed_at_diff = moment(r.pushed_at).fromNow()
-              console.log(r.pushed_at_diff)
-
-              let status = await octokit.repos.listStatusesForRef({
-                owner: 'npm',
-                repo: r.name,
-                ref: r.default_branch
-              })
-              r.build_status = status && status.data && status.data[0] ? status.data[0].state : ''
-
-              try {
-                response = await fetch(`https://coveralls.io/github/npm/${r.name}.json`)
-                stats = await response.json()
-              } catch (e) {
-                console.error(e)
-              }
-              r.coverage = stats ? Math.round(stats.covered_percent) : ''
-              r.coverageLevel = r.coverage ? (r.coverage === 100) ? 'high' : (r.coverage > 80) ? 'medium' : 'low' : ''
-
-              try {
-                response = await fetch(`https://raw.githack.com/npm/${r.name}/${r.default_branch}/package.json`)
-                pkg = await response.json()
-                if (pkg && pkg.name) {
-                  response = await fetch(`https://api.npmjs.org/downloads/point/last-month/${pkg.name}`)
-                  data = await response.json()
-                }
-              } catch (e) {
-                console.error(e)
-              }
-              r.node = pkg && pkg.engines && pkg.engines.node ? pkg.engines.node : null
-              r.license.key = pkg ? pkg.license || r.license.key : null
-              r.version = pkg ? pkg.version : null
-              r.downloads = data && data.downloads ? data.downloads : 0
-
-              return r
-
-            } catch (e) {
-              console.error(e)
-            }
-
-            return r
-
-          })).then(cb).catch(e => console.error(e))
-        } else {
-          page++
-          repositories = repositories.concat(data)
-          sleep(1500)
-          run(page, cb)
-        }
+  Promise.all(repositories.map(async _ => {
+    let r = _
+    try {
+      let response
+      let pkg
+      let stats
+      let data
+      let repo = _.repository.split('/').slice(-2)
+      let owner = repo[0]
+      let name = repo[1]
+      let prs = await octokit.pulls.list({
+        owner,
+        repo: name,
+        per_page: 100
       })
-  }
-  run(null, writeFile)
+      r = await octokit.repos.get({
+        owner,
+        repo: name
+      })
+      r = r.data
+      r.prs_count = prs.data.length || 0
+      r.prs_count = r.prs_count  === 100 ? '100+' : r.prs_count
+      r.issues_count = (r.open_issues_count || 0) - r.prs_count
+      r.pushed_at_diff = moment(r.pushed_at).fromNow()
+      try {
+        response = await fetch(`https://coveralls.io/github/${owner}/${name}.json`)
+        stats = await response.json()
+      } catch (e) {
+        console.error(e)
+      }
+      r.coverage = stats ? Math.round(stats.covered_percent) : ''
+      r.coverageLevel = r.coverage ? (r.coverage === 100) ? 'high' : (r.coverage > 80) ? 'medium' : 'low' : ''
+      try {
+        response = await fetch(`https://raw.githack.com/${owner}/${name}/${r.default_branch}/package.json`)
+        // response = await fetch(`https://unpkg.com/${_.name}/package.json`)
+        pkg = await response.json()
+        if (pkg && pkg.name) {
+          response = await fetch(`https://api.npmjs.org/downloads/point/last-month/${pkg.name}`)
+          data = await response.json()
+        }
+      } catch (e) {
+        console.error(e)
+      }
+      r.node = pkg && pkg.engines && pkg.engines.node ? pkg.engines.node : null
+      r.license.key = pkg ? pkg.license || r.license.key : null
+      r.version = pkg ? pkg.version : null
+      r.downloads = data && data.downloads ? data.downloads : 0
+    } catch (e) {
+      console.error(e)
+    }
+
+    sleep(1000)
+    return r
+
+  })).then(writeFile).catch(e => console.error(e))
+
 })()
