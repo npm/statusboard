5bd12a526f448846d550429002dd1a53db63cc38
Chris Sidi <hashtagchris@github.com>
2024-07-17 20:21:43 -0400

    chore: enable templateOSS.prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
   extends: [
     '@npmcli',
     ...localConfigs,
+    'prettier',
   ],
 }
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/* This file is automatically added by @npmcli/template-oss. Do not edit. */
+
+const githubConfig = require('@github/prettier-config')
+
+module.exports = {
+  ...githubConfig,
+  bracketSpacing: true,
+}
