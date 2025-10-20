0958fa1ace0883e5f22e0c7b915e5fe7f9982103
Michael Smith <owlstronaut@github.com>
2025-10-20 13:54:15 -0700

    chore: move to node:test

--- workspaces/data/test/index.js
+++ workspaces/data/test/index.js
@@ -1,11 +1,12 @@
-import t from 'tap'
 import fs from 'fs/promises'
+import { default as t } from 'node:test'
+import { default as a } from 'node:assert'
 import wwwPaths from 'www'
 
 t.test('no missing data', async () => {
   const latest = await fs.readFile(wwwPaths.latest, 'utf-8').then(r => JSON.parse(r))
   const maintained = await fs.readFile(wwwPaths.maintained, 'utf-8').then(r => JSON.parse(r))
 
-  t.ok(latest.length)
-  t.ok(maintained.length)
+  a.ok(latest.length)
+  a.ok(maintained.length)
 })
