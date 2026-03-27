426b1b48d93b7e65a701cadb5b7c58eb43489a14
Michael Smith <owlstronaut@github.com>
2026-03-27 08:55:18 -0700

    fix: use side-effect imports for datatables.net ESM modules (#1097)

--- workspaces/www/lib/js/datatable.js
+++ workspaces/www/lib/js/datatable.js
@@ -1,10 +1,11 @@
-import DT from 'datatables.net'
-import DTBS4 from 'datatables.net-bs4'
-import DTButtons from 'datatables.net-buttons'
-import DTButtonsBS4 from 'datatables.net-buttons-bs4'
-import ColVis from 'datatables.net-buttons/js/buttons.colVis.js'
+import $ from 'jquery'
+import 'datatables.net'
+import 'datatables.net-bs4'
+import 'datatables.net-buttons'
+import 'datatables.net-buttons-bs4'
+import 'datatables.net-buttons/js/buttons.colVis.mjs'
 
-const sumPlugin = (_, $) => $.fn.dataTable.Api.register('sum()', function () {
+$.fn.dataTable.Api.register('sum()', function () {
   return this.flatten().reduce(function (a, b) {
     if (typeof a === 'string') {
       a = a.replace(/[^\d.-]/g, '') * 1
@@ -15,12 +16,3 @@ const sumPlugin = (_, $) => $.fn.dataTable.Api.register('sum()', function () {
     return a + b
   }, 0)
 })
-
-export default (jQuery) => {
-  DT(window, jQuery)
-  DTBS4(window, jQuery)
-  DTButtons(window, jQuery)
-  DTButtonsBS4(window, jQuery)
-  ColVis(window, jQuery)
-  sumPlugin(window, jQuery)
-}
